0dd7ab7defa40e0e2b7dc9f98effb575190455ac
RamonVG8 <ramvergar@alum.us.es>
2025-02-26 16:44:23 +0100

    Mandatory 4 Done!

---
+++ src/main/java/acme/entities/bookings/Booking.java
@@ -0,0 +1,44 @@
+
+package acme.entities.bookings;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.Past;
+
+import acme.client.components.datatypes.Money;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidMoney;
+import acme.client.components.validation.ValidString;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Booking {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@Mandatory
+	@ValidString(pattern = "^[A-Z0-9]{6,8}$\\")
+	private String				locatorCode;
+
+	@Mandatory
+	@ValidMoment
+	@Past
+	private Date				pruchaseMoment;
+
+	@Mandatory
+	private TravelClass			travelClass;
+
+	@Mandatory
+	@ValidMoney
+	private Money				price;
+
+	@Optional
+	private String				lastNibble;
+
+}
