032bf57887ee6edf3bb21c18e56daa9e722ef0b7
Alejandro Nicolalde <alenicbra@alum.us.es>
2025-03-04 00:12:12 +0100

    Student #3 Mandatory 4

--- src/main/java/acme/entities/flightAssignments/FlightAssignment.java
+++ src/main/java/acme/entities/flightAssignments/FlightAssignment.java
@@ -4,14 +4,18 @@ package acme.entities.flightAssignments;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractEntity;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidMoment;
 import acme.client.components.validation.ValidString;
+import acme.entities.flightCrewMembers.FlightCrewMember;
+import acme.entities.legs.Leg;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -36,5 +40,17 @@ public class FlightAssignment extends AbstractEntity {
 	@Optional
 	@ValidString(max = 255)
 	private String				remarks;
+
+	//---------------- Relationships --------------------
+
+	@Valid
+	@Mandatory
+	@ManyToOne
+	private FlightCrewMember	flightCrewMember;
+
+	@Valid
+	@Mandatory
+	@ManyToOne
+	private Leg					leg;
 
 }
