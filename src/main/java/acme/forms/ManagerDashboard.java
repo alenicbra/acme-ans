5beb4ef148b8e11852937cbaade989a74004914b
Pablo Rodríguez Sánchez <pabrodsan@alum.us.es>
2025-04-27 20:41:35 +0200

    feat: Create Manager Dashboard

---
+++ src/main/java/acme/forms/ManagerDashboard.java
@@ -0,0 +1,27 @@
+
+package acme.forms;
+
+import java.util.Map;
+
+import acme.client.components.basis.AbstractForm;
+import acme.client.components.datatypes.Money;
+import acme.entities.airports.Airport;
+import acme.entities.legs.LegStatus;
+
+public class ManagerDashboard extends AbstractForm {
+
+	private static final long	serialVersionUID	= 1L;
+
+	Double						rankingExperience;
+	Integer						yearsToRetire;
+	Double						ratioOnTimeLegs;
+	Double						ratioDelayedLegs;
+	Airport						mostPopularAirport;
+	Airport						lessPopularAirport;
+	Map<LegStatus, Integer>		numberOfLegs;
+	Money						averageCostFlights;
+	Money						minimumCostFlights;
+	Money						maximumCostFlights;
+	Money						standardDeviationCostFlights;
+
+}
